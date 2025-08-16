d2b84477bef168c2f8b8741b4cdeb589967b21e0
shafin1196 <shafin23105101196@diu.edu.bd>
2025-08-16 18:52:56 +0000

    add static

--- api/settings.py
+++ api/settings.py
@@ -120,10 +120,13 @@ USE_I18N = True
 USE_TZ = True
 
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/5.1/howto/static-files/
+# Static files
+STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
-STATIC_URL = 'static/'
+# Media files (if you allow user uploads)
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
